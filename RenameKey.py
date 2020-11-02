47f3fbc12f82a4591969d770cb565127a3596007
Shraddha Gupta <65810405+Shraddha-713@users.noreply.github.com>
2020-11-02 20:06:30 +0530

    Create RenameKey.py

---
+++ RenameKey.py
@@ -0,0 +1,11 @@
+SkillSanta_Dict = {
+    "Name" : "Sachin",
+    "Age" : 22,
+    "Salary" : 60000,
+    "City" : "New Delhi"
+}
+print(SkillSanta_Dict)
+new_key = "Location"
+old_key = "City"
+SkillSanta_Dict[new_key] = SkillSanta_Dict[old_key]
+print(SkillSanta_Dict)
